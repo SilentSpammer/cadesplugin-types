b1f08a33df3bbfb11b87589e9dd49710021bb84c
Fallen Mireq <shipiguzov@tados.ru>
2018-10-30 18:58:54 +0500

    declare const cadesplugin

--- index.d.ts
+++ index.d.ts
@@ -3,3 +3,5 @@ import * as Sync from './interfaces.sync/@';
 import * as Async from './interfaces/@';
 
 export { Sync, Async };
+
+declare const cadesplugin: Sync.IWebClassFactory | Async.IWebClassFactory;
