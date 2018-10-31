d809bcc8bf3173de23b51ea22c11ddf4c62c7c66
Fallen Mireq <shipiguzov@tados.ru>
2018-10-31 16:18:47 +0500

    isAsync/isSync overriding

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,12 @@
-export * from './enums/@';
 import * as Sync from './interfaces.sync/@';
 import * as Async from './interfaces.async/@';
-export * from './interfaces.variant/@';
+import * as Variant from './interfaces.variant/@';
 
+export * from './enums/@';
+export * from './interfaces.variant/@';
 export { Sync, Async };
 
-export declare const cadesplugin: Sync.IWebClassFactory | Async.IWebClassFactory;
-
+export declare const cadesplugin: Variant.IWebClassFactory;
 export declare const asyncMode: boolean;
-
-export declare function isAsync(
-    cadesplugin: Sync.IWebClassFactory | Async.IWebClassFactory
-): cadesplugin is Async.IWebClassFactory;
-
-export declare function isSync(
-    cadesplugin: Sync.IWebClassFactory | Async.IWebClassFactory
-): cadesplugin is Sync.IWebClassFactory;
+export * from './guard-sync';
+export * from './guard-async';
